2e3cdcfb34b7c5554e859aad98b24c5f8c913186
Kjell Inge Rotnes <kirotnes@gmail.com>
2018-04-27 00:20:21 +0200

    Reordred x-ray setup

--- src/x-ray.js
+++ src/x-ray.js
@@ -5,23 +5,33 @@ import os from 'os';
 import http from 'http';
 
 let xRayInitialized = false;
+let configuredSamplingRules = null;
 
 export const xRayInitialize = ({ samplingRules, deamonAddress }) => {
+
+    if (xRayInitialized) throw Error('Already initialized');
+
+    configuredSamplingRules = samplingRules;
+
+    AWSXRay.setDaemonAddress(deamonAddress);
     AWSXRay.captureHTTPsGlobal(http);
-    AWSXRay.captureAWS(AWS);
-    AWSXRay.middleware.setSamplingRules(samplingRules);
-    AWSXRay.setDaemonAddress(deamonAddress);    
+    AWSXRay.captureAWS(AWS);    
 
     if (process.env.ECS_CONTAINER_METADATA_FILE) {
         AWSXRay.config([AWSXRay.plugins.ECSPlugin]);
     }
     xRayInitialized = true;
+
+    console.log('init done');
 }
 
 export const xRayOpenExpressMiddleware = (appName) => {
     const contatinerMetadata = getContainterMetadataSync();
     const segmentName = `${appName}@${contatinerMetadata ? contatinerMetadata.ContainerID.substring(0, 12) : os.hostname()}`;
-    return AWSXRay.express.openSegment(segmentName);
+    const middleware = AWSXRay.express.openSegment(segmentName);
+    AWSXRay.middleware.setSamplingRules(configuredSamplingRules);
+    console.log('sampling rules set', configuredSamplingRules);
+    return middleware;
 }
 
 export const xRayCloseExpressMiddleware = () => AWSXRay.express.closeSegment();
