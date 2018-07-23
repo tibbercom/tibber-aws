604bbcef8222ecc39d0858d529470141018f042b
Kjell Inge Rotnes <kirotnes@gmail.com>
2018-07-23 17:38:38 +0200

    Exception handling

--- src/secrets.js
+++ src/secrets.js
@@ -4,7 +4,13 @@ let data = undefined;
 
 export const getSecret = function (collection, property) {
     if (data) return data[property];
-    const client = rpc(__dirname + '/syncSecrets.js');
-    data = JSON.parse(client({ region: AWS.config.region, secret: collection }).SecretString);
+    try {
+        const client = rpc(__dirname + '/syncSecrets.js');
+        data = JSON.parse(client({ region: AWS.config.region, secret: collection }).SecretString);
+    }
+    catch (err) {
+        return undefined;
+    }
+    
     return data ? data[property] : undefined;
 }
