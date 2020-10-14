2150e3efd077e5314611bae732c1a16548842c0d
Toni Juvani <toni@tibber.com>
2020-10-14 14:30:00 +0300

    Drop dependency to aws xray

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ export { S3Bucket } from './s3';
 export { Topic } from './topic';
 export { Queue, QueueSubjectListener, QueueSubjectListenerBuilder } from './queue';
 export { getContainterMetadata, getContainterMetadataSync } from './container-info';
-export { xRayCloseExpressMiddleware, xRayInitialize, xRayIsInitialized, xRayOpenExpressMiddleware } from './x-ray';
 export { configure } from './configure';
 export { getSecret, getSecretCollection } from './secrets';
 export { getLambdaFunc } from './lambda';
